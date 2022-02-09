f714fd733dcae8c5e72e3acc4243bb3eb96f5716
max_egorov <max.egorov@sureprep.com>
2022-02-09 12:56:19 -0800

    added new files

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Max Egorov on 2/9/22.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var cityUILabel: UILabel!
+    @IBOutlet weak var textUITextView: UITextView!
+    @IBOutlet weak var dobUILabel: UILabel!
+    @IBOutlet weak var nameUILabel: UILabel!
+    @IBOutlet weak var imageUIImage: UIImageView!
+    
+}
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Max Egorov on 2/9/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
