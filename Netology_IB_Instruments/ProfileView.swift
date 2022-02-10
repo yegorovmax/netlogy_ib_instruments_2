e35458006ebc37301fcdae9db480fea600b62ce9
max_egorov <max.egorov@sureprep.com>
2022-02-09 16:46:28 -0800

    added new files

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -9,10 +9,39 @@ import UIKit
 
 class ProfileView: UIView {
     
+    
+    //@IBOutlet weak var textUIText: UITextView!
+    //@IBOutlet weak var cityUILabel: UILabel!
+    //@IBOutlet weak var dobUILabel: UILabel!
+    //@IBOutlet weak var nameUILabel: UILabel!
+    //@IBOutlet weak var imageUIImageView: UIImageView!
+    
     @IBOutlet weak var cityUILabel: UILabel!
-    @IBOutlet weak var textUITextView: UITextView!
     @IBOutlet weak var dobUILabel: UILabel!
     @IBOutlet weak var nameUILabel: UILabel!
-    @IBOutlet weak var imageUIImage: UIImageView!
-    
+    @IBOutlet weak var imageUIImageView: UIImageView!
+    @IBOutlet weak var textUIText: UITextView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setupView()
+    }
+
+    private func setupView() {
+        let view = self.loadViewFromXib()
+        self.addSubview(view)
+        view.frame = self.bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+
+    private func loadViewFromXib() -> UIView {
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView else { return UIView() }
+    return view
+    }
+
 }
